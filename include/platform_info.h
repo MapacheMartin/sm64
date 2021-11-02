c0e9b430bc0192fa08b31b2670eabfdaad9a34a6
MartinLugo <martinemmir@gmail.com>
2021-11-01 22:34:01 -0700

    first commit

---
+++ include/platform_info.h
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_INFO_H
+#define PLATFORM_INFO_H
+
+#include <stdint.h>
+#define IS_64_BIT (UINTPTR_MAX == 0xFFFFFFFFFFFFFFFFU)
+#define IS_BIG_ENDIAN (__BYTE_ORDER__ == __ORDER_BIG_ENDIAN__)
+
+// Using 8 here instead of sizeof(size_t) to ensure compatibility
+#define SIZEOF_POINTER (size_t)8
+#define DOUBLE_SIZE_ON_64_BIT(size) ((size) * (SIZEOF_POINTER / 4))
+
+#endif
